7e8a29a0f0760b95c29ec8e7d1b6bc7dcc854d97
marceloaviana <96154514+marceloaviana@users.noreply.github.com>
2023-11-07 00:50:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Exemplo
+
+def exibirMensagem():
+  frase = "Python é fácil!"
+  print(frase)
+
+#print("Início")
+#exibirMensagem()
+
+
+# Exercicio 22
+
+
+def saudacao(nome):
+  print(f"Olá {nome}, seja bem-vindo!")
+
+nome = input("Digite seu nome: ")
+saudacao(nome)
